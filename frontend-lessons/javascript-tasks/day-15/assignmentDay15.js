5ad1baf5e7cbdaf79d8d200760ffbf6f009fadcd
KristineTabs <kristinetabiolowork@gmail.com>
2024-01-20 23:54:11 +0800

    Did assignment for day 15

---
+++ frontend-lessons/javascript-tasks/day-15/assignmentDay15.js
@@ -0,0 +1,15 @@
+const cookies = ["chocolate chip", "peanut butter", "oatmeal raisin", "sugar", "blueberry walnut"];
+let jarNotEmpty = true;
+
+for (let i = 1 ; i <= 5 ; i++) {
+    console.log(i);
+}
+
+for (let i = 0 ; i < cookies.length ; i++) {
+    console.log(cookies[i] + " cookies");
+}
+
+while (jarNotEmpty) {
+    console.log("Eating " + cookies[i] + " cookies");
+    i++;
+}
