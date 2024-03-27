bba5bb64b0ed391aaab3ba85b975d977c6126c02
KristineTabs <kristinetabiolowork@gmail.com>
2024-03-27 20:07:07 +0800

    Complete codes in index.js
    I have applied the needed codes in index.js

--- projects/03-rock-paper-scissors/index.js
+++ projects/03-rock-paper-scissors/index.js
@@ -1,36 +1,185 @@
-// Step 1: Define the choices available in the game
+// Array of the hands
 const choices = ["rock", "paper", "scissors"];
 
-// Step 2: Get references to HTML elements
-const player1ChoiceDisplay = // Get the element with the id of "player1-choice-display"
-const player1ScoreDisplay = // Get the element with the id of "player1-score"
-const player2ChoiceDisplay = // Get the element with the id of "player2-choice-display"
-const player2ScoreDisplay = // Get the element with the id of "player2-score"
+// Assigns pictures to each element in the array
+choices[0] = "assets/hand-rock-solid.svg";
+choices[1] = "assets/hand-paper-solid.svg";
+choices[2] = "assets/hand-scissors-solid.svg";
 
-const gameStatusDisplay = // Get the element with the id of "game-status"
-const resetGameButton = // Get the element with the id of "reset-game-btn"
+// Image of the hand is displayed here
+const player1ChoiceDisplay = document.getElementById("player1-choice-display");
 
-const rockBtn = // Get the element with the id of "rock-btn"
-const paperBtn = // Get the element with the id of "paper-btn"
-const scissorsBtn = // Get the element with the id of "scissors-btn"
+// Score of Player 1 is displayed here
+const player1ScoreDisplay = document.getElementById("player1-score");
 
-// Step 3: Add event listeners to the buttons
-rockBtn.addEventListener("click", function () {
+// Image of the hand is displayed here
+const player2ChoiceDisplay = document.getElementById("player2-choice-display");
 
+// Score of Player 2 is displayed here
+const player2ScoreDisplay = document.getElementById("player2-score");
+
+// Status of the game is displayed here
+const gameStatusDisplay = document.getElementById("game-status");
+
+// The Reset Button
+const resetGameButton = document.getElementById("reset-game-btn");
+
+// Adds image and id to the rock button
+const rockBtn = document.getElementById("rock-btn");
+const rockImg = document.createElement("img");
+
+// Adds image and id to the paper button
+const paperBtn = document.getElementById("paper-btn");
+const paperImg = document.createElement("img");
+
+// Adds image and id to the scissors button
+const scissorsBtn = document.getElementById("scissors-btn");
+const scissorsImg = document.createElement("img");
+
+// Creates img element to be shown in choice-display
+const player1Hand = document.createElement("img");
+const player2Hand = document.createElement("img");
+
+// Initializes both scores
+let player1Score = 0;
+let player2Score = 0;
+
+// When Rock button is clicked
+rockBtn.addEventListener("click", function() {
+
+    if (confirm("Your hand is rock?") == true) {
+
+        let rock = 1; // For first click
+        if (rock > 1) { // Checks if rock-btn is clicked more than once
+            player1ChoiceDisplay.removeChild(player1Hand);
+            player2ChoiceDisplay.removeChild(player2Hand);
+        }
+
+        // Puts rock picture in player1-choice-display
+        player1Hand.setAttribute("src", "assets/hand-rock-solid.svg");
+        player1Hand.setAttribute("alt", "Hand Rock");
+        player1ChoiceDisplay.appendChild(player1Hand);
+
+        // Randomize player 2's hand
+        const randomChoice = Math.floor(Math.random() * choices.length);
+        player2Hand.setAttribute("src", choices[randomChoice]);
+        player2ChoiceDisplay.appendChild(player2Hand);
+
+        // If player 2's hand is scissors
+        if (player2Hand.getAttribute("src") == choices[2]) {
+            gameStatusDisplay.textContent = "PLAYER 1 WINS";
+            player1Score++;
+            player1ScoreDisplay.textContent = player1Score;
+        }
+
+        // If player 2's hand is paper
+        else if (player2Hand.getAttribute("src") == choices[1]) {
+            gameStatusDisplay.textContent = "PLAYER 2 WINS";
+            player2Score++;
+            player2ScoreDisplay.textContent = player2Score;
+        }
+
+        // If player 2's hand is rock
+        else if (player2Hand.getAttribute("src") == choices[0]) {
+            gameStatusDisplay.textContent = "DRAW";
+        }
+
+        // Increments everytime it's clicked
+        rock++;
+    }
 });
 
-paperBtn.addEventListener("click", function () {
+// When Paper button is clicked
+paperBtn.addEventListener("click", function() {
 
+    if (confirm("Your hand is paper?") == true) {
+
+        let paper = 1; // For first click
+        if (paper > 1) { // Checks if rock-btn is clicked more than once
+            player1ChoiceDisplay.removeChild(player1Hand);
+            player2ChoiceDisplay.removeChild(player2Hand);
+        }
+
+        // Puts rock picture in player1-choice-display
+        player1Hand.setAttribute("src", "assets/hand-paper-solid.svg");
+        player1Hand.setAttribute("alt", "Hand Paper");
+        player1ChoiceDisplay.appendChild(player1Hand);
+
+        // Randomize player 2's hand
+        const randomChoice = Math.floor(Math.random() * choices.length);
+        player2Hand.setAttribute("src", choices[randomChoice]);
+        player2ChoiceDisplay.appendChild(player2Hand);
+
+        // If player 2's hand is rock
+        if (player2Hand.getAttribute("src") == choices[0]) {
+            gameStatusDisplay.textContent = "PLAYER 1 WINS";
+            player1Score++;
+            player1ScoreDisplay.textContent = player1Score;
+        }
+
+        // If player 2's hand is scissors
+        else if (player2Hand.getAttribute("src") == choices[2]) {
+            gameStatusDisplay.textContent = "PLAYER 2 WINS";
+            player2Score++;
+            player2ScoreDisplay.textContent = player2Score;
+        }
+
+        // If player 2's hand is paper
+        else if (player2Hand.getAttribute("src") == choices[1]) {
+            gameStatusDisplay.textContent = "DRAW";
+        }
+
+        // Increments everytime it's clicked
+        paper++;
+    }
 });
 
+// When Scissors button is clicked
 scissorsBtn.addEventListener("click", function () {
 
+    if (confirm("Your hand is scissors?") == true) {
+
+        let scissors = 1; // For first click
+        if (scissors > 1) { // Checks if rock-btn is clicked more than once
+            player1ChoiceDisplay.removeChild(player1Hand);
+            player2ChoiceDisplay.removeChild(player2Hand);
+        }
+
+        // Puts rock picture in player1-choice-display
+        player1Hand.setAttribute("src", "assets/hand-scissors-solid.svg");
+        player1Hand.setAttribute("alt", "Hand Scissors");
+        player1ChoiceDisplay.appendChild(player1Hand);
+
+        // Randomize player 2's hand
+        const randomChoice = Math.floor(Math.random() * choices.length);
+        player2Hand.setAttribute("src", choices[randomChoice]);
+        player2ChoiceDisplay.appendChild(player2Hand);
+
+        // If player 2's hand is paper
+        if (player2Hand.getAttribute("src") == choices[1]) {
+            gameStatusDisplay.textContent = "PLAYER 1 WINS";
+            player1Score++;
+            player1ScoreDisplay.textContent = player1Score;
+        }
+
+        // If player 2's hand is rock
+        else if (player2Hand.getAttribute("src") == choices[0]) {
+            gameStatusDisplay.textContent = "PLAYER 2 WINS";
+            player2Score++;
+            player2ScoreDisplay.textContent = player2Score;
+        }
+
+        // If player 2's hand is scissors
+        else if (player2Hand.getAttribute("src") == choices[2]) {
+            gameStatusDisplay.textContent = "DRAW";
+        }
+
+        // Increments everytime it's clicked
+        scissors++;
+    }
 });
 
+// When Reset Button is clicked
 resetGameButton.addEventListener("click", function () { 
-    
+    location.reload();
 });
-
-// Step 4: Define helper functions that will be the actions of the game
-
-// Step 5: Embrace mistakes as opportunities for learning and improvement, and fuel your curiosity by exploring new the wonders of frontend development. The vast tech world is filled with exciting discoveries waiting for you. Ask when you have the need or want to! Just keep coding!
